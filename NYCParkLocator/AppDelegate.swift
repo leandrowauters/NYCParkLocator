9fbca96f95935c3a07c3d90e48a94a4035861c41
Leandro Wauters <leandro.wauters@gmail.com>
2021-08-20 13:35:35 -0400

    Test

---
+++ NYCParkLocator/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  NYCParkLocator
+//
+//  Created by Leandro Wauters on 8/20/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.fgf
+        return true
+    }
+
+
+}
+
