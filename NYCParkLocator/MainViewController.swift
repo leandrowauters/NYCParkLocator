a0c5e76f77bdc9565f62cfb235e3bdb1998bec5b
Leandro Wauters <leandro.wauters@gmail.com>
2021-08-20 16:39:16 -0400

    Map is setup with user location

--- NYCParkLocator/MainViewController.swift
+++ NYCParkLocator/MainViewController.swift
@@ -6,26 +6,118 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, CLLocationManagerDelegate {
 
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    private var locationManager = CLLocationManager()
+    
+    private var userCoordinate: CLLocationCoordinate2D?
+    let defultCoordinate = CLLocationCoordinate2D(latitude: 40.7484, longitude: -73.9857)
+    
+    let regionRadius: Double = 1500
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        requestLocationPersmissions()
+        setupMapView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func requestLocationPersmissions() {
+      locationManager.requestWhenInUseAuthorization()
     }
-    */
+    
+    private func userLocationButtonHandler() {
+        //TODO: SHOW ALERT WHEN DENIED. SHOW USER LOCATION WHEN APPROVED
+        
 
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways:
+          print("authorizedAlways")
+            guard let userCoordinate = self.userCoordinate else {
+                return
+            }
+          centerMapOnUserLocation(coordinate: userCoordinate)
+        case .authorizedWhenInUse:
+          print("authorizedWhenInUse")
+            guard let userCoordinate = self.userCoordinate else {
+                return
+            }
+          centerMapOnUserLocation(coordinate: userCoordinate)
+        case .denied, .notDetermined, .restricted:
+          print("denied")
+            showAlert(title: "User Location Status: Denied", message: "Open Settings?") { alertAction in
+                self.showAppSettings()
+            }
+        default:
+          break
+        }
+    }
+    private func setupMapView() {
+        mapView.showsUserLocation = true
+       
+    }
+    
+    private func showAppSettings() {
+        if let url = URL(string:UIApplication.openSettingsURLString) {
+            if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    public func showAlert(title: String?, message: String?, handler: ((UIAlertAction) -> Void)?) {
+      let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+      let okAction = UIAlertAction(title: "Ok", style: .default, handler: handler)
+
+        alertController.addAction(okAction)
+        alertController.addAction(cancel)
+        
+      present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func didPressMyLocation(_ sender: Any) {
+        userLocationButtonHandler()
+    }
+    
+    func centerMapOnUserLocation(coordinate: CLLocationCoordinate2D) {
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+        switch status {
+        case .authorizedAlways:
+          print("authorizedAlways")
+          locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+          print("authorizedWhenInUse")
+          locationManager.startUpdatingLocation()
+        case .denied, .notDetermined, .restricted:
+          print("denied")
+            
+
+            centerMapOnUserLocation(coordinate: defultCoordinate)
+        default:
+          break
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.last else {return}
+        userCoordinate = currentLocation.coordinate
+        guard let userCoordinate = self.userCoordinate else {
+            print("NO USER COORDINATE FOUNT")
+            return
+        }
+        centerMapOnUserLocation(coordinate: userCoordinate)
+    }
 }
